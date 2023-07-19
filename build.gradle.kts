23ab7a90a93743a6e0e4d1a84d1413fb3e86acf8
Yuri <yuridiego17@gmail.com>
2023-07-19 00:15:39 -0300

    Commit1

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "br.com.ntconsult"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+    testImplementation("org.testng:testng:7.1.0")
+
+
+
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
